2dc0f3c3324cf1e89cb7f4f0f4a32c88018896ed
Francisco Abarza <francisco@maskin.cl>
2017-08-24 19:14:19 -0300

    add button animation wip

--- app/src/main/java/com/beetrack/test/abarza/beelogin/login/LoginActivity.java
+++ app/src/main/java/com/beetrack/test/abarza/beelogin/login/LoginActivity.java
@@ -1,29 +1,37 @@
 package com.beetrack.test.abarza.beelogin.login;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.beetrack.test.abarza.beelogin.R;
 
+
+
 public class LoginActivity extends AppCompatActivity {
 
+  private static final String TAG = LoginActivity.class.getSimpleName();
   private EditText mEditTextUserName;
   private EditText mEditTextPassword;
   private TextInputLayout mTextInputUserName;
   private TextInputLayout mTextInputPassword;
   private Button mLoginButton;
   private ProgressBar mLoadingLogin;
+  private RelativeLayout mCheckLogin;
+  private TextView mLoginText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
     mLoginButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        validate();
+        login();
       }
     });
 
@@ -49,6 +57,48 @@ public class LoginActivity extends AppCompatActivity {
     mEditTextPassword = (EditText) findViewById(R.id.passwordEditText);
     mLoginButton = (Button) findViewById(R.id.login_button);
     mLoadingLogin = (ProgressBar) findViewById(R.id.loadingLogin);
+    mCheckLogin = (RelativeLayout) findViewById(R.id.checkLogin);
+    mLoginText = (TextView) findViewById(R.id.textLogin);
+
+  }
+
+  private void login() {
+    validate();
+    loading();
+  }
+
+  private void setAnimation(final View view, int currentWidth, int newWidth) {
+
+    ValueAnimator widthAnimator = ValueAnimator
+        .ofInt(currentWidth, newWidth)
+        .setDuration(1000);
+
+    widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+
+        // get the value the interpolator is at
+        Integer value = (Integer) animation.getAnimatedValue();
+
+        view.getLayoutParams().width = value;
+        view.requestLayout();
+      }
+    });
+
+    AnimatorSet set = new AnimatorSet();
+    set.play(widthAnimator);
+    // this is how you set the parabola which controls acceleration
+    set.setInterpolator(new AccelerateDecelerateInterpolator());
+    // start the animation
+    set.start();
+
+  }
+
+  private void loading() {
+    setAnimation(mCheckLogin,500, 200);
+    mLoginText.setVisibility(View.GONE);
+    mLoadingLogin.setVisibility(View.VISIBLE);
+
   }
 
   public void validate() {
@@ -65,9 +115,6 @@ public class LoginActivity extends AppCompatActivity {
       passError = getString(R.string.required_field);
     }
     toggleTextInputLayoutError(mTextInputPassword, passError);
-
-
-    //TODO: check if fields are valid
 
 
     // hide keyboard
