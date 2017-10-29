39db93ff9674e0926fce29b7c0e9d75b7038b3a6
Francisco Abarza <abarza@abarza.local>
2017-10-29 15:58:18 -0300

    rename some styles

--- app/src/androidTest/java/com/frontend/test/abarza/beelogin/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/frontend/test/abarza/beelogin/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("com.beetrack.test.abarza.beelogin", appContext.getPackageName());
+    assertEquals("com.frontend.test.abarza.beelogin", appContext.getPackageName());
   }
 }
